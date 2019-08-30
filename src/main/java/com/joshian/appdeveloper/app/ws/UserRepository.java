aa8d34174e74c35900e6e679bc375495037ce65b
anujvjoshi <anujvjoshi@live.com>
2019-08-30 17:08:22 +0530

    adding new updates for Repo

--- src/main/java/com/joshian/appdeveloper/app/ws/UserRepository.java
+++ src/main/java/com/joshian/appdeveloper/app/ws/UserRepository.java
@@ -1,11 +1,11 @@
 package com.joshian.appdeveloper.app.ws;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.joshian.appdeveloper.app.ws.io.entity.UserEntity;
 
-@Repository
+@Service
 public interface UserRepository extends CrudRepository<UserEntity, Long>{
 	UserEntity findUserByEmail(String email);
 	UserEntity findUserbyFirstName(String firstName);
--- src/main/java/com/joshian/appdeveloper/app/ws/service/UserService.java
+++ src/main/java/com/joshian/appdeveloper/app/ws/service/UserService.java
@@ -1,5 +1,7 @@
 package com.joshian.appdeveloper.app.ws.service;
 
+import org.springframework.stereotype.Service;
+
 import com.joshian.appdeveloper.app.ws.shared.dto.UserDto;
 
 public interface UserService {
