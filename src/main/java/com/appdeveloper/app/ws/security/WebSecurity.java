ebe048cb4381d1c1629fc1cc7024d011551dba7d
Anuj Joshi <anujvjoshi@live.com>
2019-09-08 12:20:57 +0530

    Update WebSecurity.java
    Formatting changes for reability

--- src/main/java/com/appdeveloper/app/ws/security/WebSecurity.java
+++ src/main/java/com/appdeveloper/app/ws/security/WebSecurity.java
@@ -22,8 +22,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/user").permitAll().anyRequest()
-				.authenticated();
+		http.csrf().disable()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/user")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
 	}
 
 	@Override
