8e0b52514380f00e54c409a2b7a8ed93d41f89bc
anujvjoshi <49179179+anujvjoshi@users.noreply.github.com>
2019-09-13 15:38:05 +0530

    Added the authorization filter and json web token

--- src/main/java/com/appdeveloper/app/ws/UserRepository.java
+++ src/main/java/com/appdeveloper/app/ws/UserRepository.java
@@ -1,7 +1,7 @@
 package com.appdeveloper.app.ws;
 
-import org.springframework.data.repository.CrudRepository;
 import com.appdeveloper.app.ws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long>{
 	UserEntity findByEmail(String email);
